72a662ca5e723af801cf7cc1070b89927abd24dd
Ludvig Hozman <ludvig123@gmail.com>
2019-04-03 16:20:22 +0200

    finalize algorithm

--- zumo/zumo.ino
+++ zumo/zumo.ino
@@ -9,22 +9,22 @@
 // constants used to tweak the behaviour
 
 #define NUM_SENSORS 6 // does not need changing!
-#define QTR_THRESHOLD 1500 // this may need to be tweaked
+#define QTR_THRESHOLD 500 // this may need to be tweaked
 // 2000: dark  0: light
 
 #define ATTACK_SPEED 400
 #define AVOID_SPEED 250
-#define TURN_DUR 300
+#define TURN_DUR 400
 #define TURN_REVERSE_DUR 200
 #define TIME_TO_TURN 2 // change this to change how long it takes to turn toward an item
-#define MAX_ATTACK_DIST 40 // max distance object is before it will attack
-#define MIN_ATTACK_DIST 10 // min distance for attack (if it does not have time, avoid!)
+#define MAX_ATTACK_DIST 30 // max distance object is before it will attack
+#define MIN_ATTACK_DIST 3 // min distance for attack (if it does not have time, avoid!)
 
 #define SONAR_NUM 4 // number of sensors
-#define PING_INTERVAL 40
+#define PING_INTERVAL 50
 
 
-#define MAX_PING_DISTANCE 100
+#define MAX_PING_DISTANCE 150
 #define SERVO_PIN 6
 
 
@@ -46,14 +46,14 @@ ZumoMotors motors;
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 
 
-void turnLeft(){
+void turnLeft(int dur){
   motors.setSpeeds(-AVOID_SPEED, AVOID_SPEED);
-  delay(500);
+  delay(dur);
 }
 
-void turnRight(){
+void turnRight(int dur){
   motors.setSpeeds(AVOID_SPEED, -AVOID_SPEED);
-  delay(500);
+  delay(dur);
 }
 
 void reverse(int time){
@@ -61,59 +61,74 @@ void reverse(int time){
   delay(time);
 }
 
-// turn towards direction dir, and full speed for time based on distance dist
-void turnToAttack(int dir, int dist){
-  // This is the diection relative to current driving direction
-  if (dir == 1) {
-    turnRight(100);
-  } else if(dir == 2) {
-    turnLeft(200);
-  } else {
-    turnLeft(100);
-  }
-  motors.setSpeeds(ATTACK_SPEED, ATTACK_SPEED);
-  delay(400);
-  motors.setSpeeds(0,0);
-}
-
-void avoidManouver(int dir, int dist) {
-  if (dir == 0) turnToAttack(0, dist);
-  else if (dir == 1) turnRight(200);
-  else if (dir == 2) turnLeft(400);
-  else turnLeft(200);
-}
-
 void keepInLine(unsigned sensor_values[NUM_SENSORS]){
-  if (sensor_values[0] > QTR_THRESHOLD){
+  if (sensor_values[0] < QTR_THRESHOLD){
     motors.setSpeeds(0,0);
     reverse(TURN_REVERSE_DUR);
-    turnRight();
-  } else if (sensor_values[5] > QTR_THRESHOLD){
+    turnRight(TURN_DUR);
+  } else if (sensor_values[5] < QTR_THRESHOLD){
     motors.setSpeeds(0,0);
     reverse(TURN_REVERSE_DUR);
-    turnLeft();
+    turnLeft(TURN_DUR);
   } else {
     motors.setSpeeds(AVOID_SPEED, AVOID_SPEED);
   }
 }
 
 
+void keepInLineinAttack(unsigned sensor_values[NUM_SENSORS]) {
+  if (sensor_values[0] < QTR_THRESHOLD) {
+    motors.setSpeeds(0,0);
+    reverse(TURN_REVERSE_DUR);
+    turnRight(TURN_DUR);
+  } else if (sensor_values[5] < QTR_THRESHOLD){
+    motors.setSpeeds(0,0);
+    reverse(TURN_REVERSE_DUR);
+    turnLeft(TURN_DUR);
+  }
+}
+
+
+
+// turn towards direction dir, and full speed for time based on distance dist
+void turnToAttack(int dir, int dist){
+  // This is the diection relative to current driving direction
+  if (dir == 1) {
+    turnRight(300);
+  } else if(dir == 2) {
+    turnLeft(550);
+  } else if(dir == 3) {
+    turnLeft(300);
+  }
+  motors.setSpeeds(0,0);
+  for (int i=0; i < dist; i++) {
+    sensors.read(sensor_values);
+    keepInLineinAttack(sensor_values);
+    motors.setSpeeds(ATTACK_SPEED, ATTACK_SPEED);
+    delay(20);
+  }
+}
+
+void avoidManouver(int dir, int dist) {
+  if (dir == 0) turnToAttack(0, dist);
+  else if (dir == 1) turnRight(100);
+  else if (dir == 2) turnLeft(200);
+  else turnLeft(100);
+  for (int i=0; i< 5*dist; i++) {
+    sensors.read(sensor_values);
+    keepInLineinAttack(sensor_values);
+    motors.setSpeeds(ATTACK_SPEED, ATTACK_SPEED);
+    delay(20);
+  }
+}
+
+
 // Basically attack everything
 void driveToObject(int dir, int dist){
-  //TODO add function which return distances relative to zumo
-  /*for (int i = 0; i < SPIN_SENSOR_NUM; i++) {*/
-      /*spinSensor_dist = spinSensor_values[i];*/
-    /*if (spinSensor_dist > MIN_ATTACK_DIST && spinSensor_dist < MIN_ATTACK_DIST){*/
-      /*turnToAttack(i, spinSensor_dist);*/
-      /*return;*/
-    /*}*/
-  /*}*/
-
   if (dist > MIN_ATTACK_DIST && dist < MAX_ATTACK_DIST) {
     turnToAttack(dir, dist);
-  } else if (dist < MIN_ATTACK_DIST) {
-   // TODO avoidManouver(dir);
-    return;
+  } else if (dist != 0 && dist < MIN_ATTACK_DIST) {
+   avoidManouver(dir, dist);
   }
 }
 
@@ -124,7 +139,11 @@ void echoCheck() {
 
     distances[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
     Serial.println("Sonar echoCheck:  ");
+    Serial.print(currentSensor);
+    Serial.print(" value:  ");
+    Serial.print(sonar[currentSensor].ping_result);
     Serial.print(" ");
+    Serial.println();
   }
 }
 
@@ -133,7 +152,7 @@ void distUpdate() {
   for (uint8_t i=0; i< SONAR_NUM; i++) {
     Serial.print(distances[i]);
       Serial.print(" ");
-    driveToObject(i, distances[i]);
+   driveToObject(i, distances[i]);
   }
   Serial.println();
 }
@@ -142,10 +161,9 @@ void distUpdate() {
 void setup(){
   Serial.begin(9600);
   sensors.init();
-  minServo.attach(SERVO_PIN);
   pingTimer[0] = millis() + 75;
   for (uint8_t i=1; i< SONAR_NUM; i++) {
-    pingTimer[i] = pingTimer[i+1] + PING_INTERVAL;
+    pingTimer[i] = pingTimer[i-1] + PING_INTERVAL;
   }
   button.waitForButton();
 
@@ -170,6 +188,6 @@ void loop(){
   /*Serial.print(sensor_values[5]);*/
   /*Serial.println();*/
 
-  //keepInLine(sensor_values);
+  keepInLine(sensor_values);
 
 }
